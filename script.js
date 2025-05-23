7e37323d776ae0d4d7c64cbca8d3071ed2c1ce5f
Selenachow <59427655+Selenachow@users.noreply.github.com>
2025-05-23 15:24:22 -0700

    update website without php

---
+++ script.js
@@ -0,0 +1,29 @@
+// document.getElementById("hi").addEventListener("mouseover", (self) => {
+//     self.textContent = "About";
+// })
+
+
+document.querySelector(".heroTextTyping").addEventListener("animationend", () => {
+    
+    setTimeout(() => {    
+
+    let navigation = document.querySelector(".heroNavigation");
+    navigation.style.color = "grey";
+    
+    let headerText = document.querySelector(".heroNavigation").style.display = "flex";
+
+    let aboutNav = document.getElementById("about");
+    let projectsNav = document.getElementById("projects");
+    let contactNav = document.getElementById("contact");
+    let linkedinNav = document.getElementById("linkedin")
+
+    aboutNav.id = "aboutDone";
+    projectsNav.id = "projectsDone";
+    contactNav.id = "contactDone";
+    linkedinNav.id = "linkedinDone"
+
+    }, 200)
+
+    
+});
+
